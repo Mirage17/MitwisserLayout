b8b601090d41d7a104845405c0314affd1778a07
David Grajales <david.grajalesc@udea.edu.co>
2020-07-07 17:42:03 -0500

    cambio menor de colores

--- app/src/main/java/com/davidgrajales/mitwisserlayout/Login.kt
+++ app/src/main/java/com/davidgrajales/mitwisserlayout/Login.kt
@@ -39,7 +39,7 @@ class Login : AppCompatActivity() {
             FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
             //.addOnCompleteListener()
             //.add*/
-
+ 
 
         }
 
